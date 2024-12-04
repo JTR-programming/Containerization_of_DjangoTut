5a9000e1bfd547e44cbaf428874689cd53c256a8
JTR-programming <69212793+JTR-programming@users.noreply.github.com>
2024-12-04 14:41:35 -0600

    Update settings.py

--- dockerizingPolls/mysite/settings.py
+++ dockerizingPolls/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q-hx*e-u&v2mlt0$0qq0k#rjr%e+q0b#2rq1ko@)mw@c^nxn$0'
+SECRET_KEY = '******************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
